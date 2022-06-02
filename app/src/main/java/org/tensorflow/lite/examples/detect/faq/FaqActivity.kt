0e94b3c8544e69a89a5e9e96ca1f622b7c6ccdd2
Ned raZZZ <smg9972@gmail.com>
2022-06-02 22:12:25 +0900

    profile activity design

--- app/src/main/java/org/tensorflow/lite/examples/detect/faq/FaqActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/detect/faq/FaqActivity.kt
@@ -6,13 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.petleader.faq.FaqRVAdapter
 import org.tensorflow.lite.examples.detect.R
-import org.tensorflow.lite.examples.detect.faq.FAQData
 
 class FaqActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faq)
 
+
+        val myBuffer: MutableList<Byte> = mutableListOf<Byte>()
         val items = mutableListOf<FAQData>()
         items.add(FAQData("하네스도 인식하나요", "인식합니다. 검사 페이지에서 목줄 인식이 하네스 인식도 동시에 진행하고 있습니다."))
         items.add(FAQData("회원가입이 되지 않습니다", "회원가입이 되지 않는다면, 해당 연락처로 연락을 주시면 해결해드리겠습니다."))

92dc09372004587deb188f5bdb5a5bd5eff5346a
Ned raZZZ <smg9972@gmail.com>
2022-06-02 22:50:33 +0900

    design HomeActivity.kt

--- app/src/main/java/org/tensorflow/lite/examples/detect/HomeActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/detect/HomeActivity.kt
@@ -1,29 +1,42 @@
 package org.tensorflow.lite.examples.detect
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.widget.ImageView
 import org.tensorflow.lite.examples.detect.faq.FaqActivity
+import org.tensorflow.lite.examples.detect.profile.ProfileActivity
 import org.tensorflow.lite.examples.detect.yolov5.DetectorActivity
 
 class HomeActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
 
-        val faqBtn = findViewById<Button>(R.id.faqBtn)
-        val camBtn = findViewById<Button>(R.id.camBtn)
+        val camBtn = findViewById<ImageView>(R.id.home_detect_button)
+        val faqBtn = findViewById<ImageView>(R.id.home_faq_button)
+        val profileBtn = findViewById<ImageView>(R.id.home_profile_button)
 
         faqBtn.setOnClickListener {
             val faqIntent = Intent(this, FaqActivity::class.java)
+            Log.d("click", "faq")
             startActivity(faqIntent)
         }
 
         camBtn.setOnClickListener {
             val camIntent = Intent(this, DetectorActivity::class.java)
+            Log.d("click", "detect")
             startActivity(camIntent)
+        }
+
+        profileBtn.setOnClickListener {
+            val profileIntent = Intent(this, ProfileActivity::class.java)
+            startActivity(profileIntent)
+            Log.d("click", "profile")
         }
     }
 }

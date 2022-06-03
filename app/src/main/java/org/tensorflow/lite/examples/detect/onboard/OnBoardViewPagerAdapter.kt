3ed00e020443363ed11c49037279953d603aad51
sectionr0 <sectionr0@gmail.com>
2022-06-03 18:23:33 +0900

    온보드 제작

---
+++ app/src/main/java/org/tensorflow/lite/examples/detect/onboard/OnBoardViewPagerAdapter.kt
@@ -0,0 +1,45 @@
+package org.tensorflow.lite.examples.detect.onboard
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+import org.tensorflow.lite.examples.detect.R
+
+class OnBoardViewPagerAdapter(
+    private var context: Context,
+    private var onBoardingDataList: List<OnBoardingData>
+) : PagerAdapter() {
+
+    override fun getCount(): Int {
+        return onBoardingDataList.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view == `object`
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view = LayoutInflater.from(context).inflate(R.layout.onboard_screen, null)
+
+        val imageView: ImageView = view.findViewById(R.id.image_onboard)
+        val title: TextView = view.findViewById(R.id.tv_title_onboard)
+        val desc: TextView = view.findViewById(R.id.tv_desc_onboard)
+        val subtitle: TextView = view.findViewById(R.id.tv_subtitle_onboard)
+
+        imageView.setImageResource(onBoardingDataList[position].imageUrl)
+        title.text = onBoardingDataList[position].title
+        desc.text = onBoardingDataList[position].desc
+        subtitle.text = onBoardingDataList[position].subtitle
+
+        container.addView(view)
+        return view
+    }
+}

48634d692c645a3d4790575ff3f15869bdffa017
sectionr0 <sectionr0@gmail.com>
2022-06-04 16:13:33 +0900

    게시판 추가
    - 게시판 틀 작업

---
+++ app/src/main/java/org/tensorflow/lite/examples/detect/community/CommunityActivity.kt
@@ -0,0 +1,31 @@
+package org.tensorflow.lite.examples.detect.community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import org.tensorflow.lite.examples.detect.R
+
+class CommunityActivity : AppCompatActivity() {
+    private val tabTitles = arrayListOf("자유", "관리", "산책", "자랑")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community)
+
+        val viewPager2 = findViewById<ViewPager2>(R.id.viewpage2_comm)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout_comm)
+        //val appbar = findViewById<AppBarLayout>(R.id.appbar)
+
+        // 뷰 페이지
+        viewPager2.adapter = PostActivityStateAdapter(this)
+
+
+        // Tablayout에 문자 추가
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            tab.text = tabTitles[position]
+
+        }.attach()
+    }
+}
---
+++ app/src/main/java/org/tensorflow/lite/examples/detect/community/CommunityFragmentList1.kt
@@ -0,0 +1,20 @@
+package org.tensorflow.lite.examples.detect.community
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.tensorflow.lite.examples.detect.R
+
+class CommunityFragmentList1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_community_list1, container, false)
+    }
+
+}

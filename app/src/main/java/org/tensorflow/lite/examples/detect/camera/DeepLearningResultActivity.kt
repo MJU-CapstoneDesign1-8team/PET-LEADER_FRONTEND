a69dfe0d9fe1d8842213cb194f40426e1640cd17
sectionr0 <sectionr0@gmail.com>
2022-06-06 03:08:07 +0900

    이미지 저장 기능 구현

--- app/src/main/java/org/tensorflow/lite/examples/detect/camera/DeepLearningResultActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/detect/camera/DeepLearningResultActivity.kt
@@ -1,10 +1,22 @@
 package org.tensorflow.lite.examples.detect.camera
 
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import okhttp3.ResponseBody
 import org.tensorflow.lite.examples.detect.R
 import org.tensorflow.lite.examples.detect.network.FlaskApi
@@ -12,15 +24,22 @@ import org.tensorflow.lite.examples.detect.network.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class DeepLearningResultActivity : AppCompatActivity() {
+//    var imageUri: Uri? = null
+    lateinit var BitmapImg : Bitmap
     private val api = RetrofitClient.create(FlaskApi::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dog_breed_result)
+        setContentView(R.layout.activity_dog_result)
 
         val resultStringBreed: String? = intent.getStringExtra("resultStringBreedUri")
         val img = findViewById<ImageView>(R.id.resultBreedImg)
+        val saveBtn = findViewById<Button>(R.id.saveImg)
 
         if (resultStringBreed != null) {
             api.getImage(resultStringBreed)
@@ -29,6 +48,7 @@ class DeepLearningResultActivity : AppCompatActivity() {
                         val toString = response.body()?.byteStream()
 
                         val decodeStream = BitmapFactory.decodeStream(toString)
+                        BitmapImg = decodeStream
                         img.setImageBitmap(decodeStream)
 
                     }
@@ -36,6 +56,121 @@ class DeepLearningResultActivity : AppCompatActivity() {
                         Log.d("로그 fail", t.message.toString())
                     }
                 })
+        }
+
+        // 이미지 저장
+        saveBtn.setOnClickListener {
+            imgSaveOnClick(BitmapImg)
+        }
+    }
+
+
+
+    fun imgSaveOnClick(bitmaps: Bitmap) {
+        val bitmap = bitmaps
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Q 버전 이상일 경우. (안드로이드 10, API 29 이상일 경우)
+            saveImageOnAboveAndroidQ(bitmap)
+            Toast.makeText(baseContext, "이미지 저장이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            // Q 버전 이하일 경우. 저장소 권한을 얻어온다.
+            val writePermission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+            if(writePermission == PackageManager.PERMISSION_GRANTED) {
+                saveImageOnUnderAndroidQ(bitmap)
+                Toast.makeText(baseContext, "이미지 저장이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                val requestExternalStorageCode = 1
+
+                val permissionStorage = arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+
+                ActivityCompat.requestPermissions(this, permissionStorage, requestExternalStorageCode)
+            }
+        }
+
+    }
+
+
+    //Android Q (Android 10, API 29 이상에서는 이 메서드를 통해서 이미지를 저장한다.)
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun saveImageOnAboveAndroidQ(bitmap: Bitmap) {
+        val fileName = System.currentTimeMillis().toString() + ".png" // 파일이름 현재시간.png
+
+        /*
+        * ContentValues() 객체 생성.
+        * ContentValues는 ContentResolver가 처리할 수 있는 값을 저장해둘 목적으로 사용된다.
+        * */
+        val contentValues = ContentValues()
+        contentValues.apply {
+            put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/ImageSave") // 경로 설정
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName) // 파일이름을 put해준다.
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+            put(MediaStore.Images.Media.IS_PENDING, 1) // 현재 is_pending 상태임을 만들어준다.
+            // 다른 곳에서 이 데이터를 요구하면 무시하라는 의미로, 해당 저장소를 독점할 수 있다.
+        }
+
+        // 이미지를 저장할 uri를 미리 설정해놓는다.
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        try {
+            if(uri != null) {
+                val image = contentResolver.openFileDescriptor(uri, "w", null)
+                // write 모드로 file을 open한다.
+
+                if(image != null) {
+                    val fos = FileOutputStream(image.fileDescriptor)
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+                    //비트맵을 FileOutputStream를 통해 compress한다.
+                    fos.close()
+
+                    contentValues.clear()
+                    contentValues.put(MediaStore.Images.Media.IS_PENDING, 0) // 저장소 독점을 해제한다.
+                    contentResolver.update(uri, contentValues, null, null)
+                }
+            }
+        } catch(e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun saveImageOnUnderAndroidQ(bitmap: Bitmap) {
+        val fileName = System.currentTimeMillis().toString() + ".png"
+        val externalStorage = Environment.getExternalStorageDirectory().absolutePath
+        val path = "$externalStorage/DCIM/imageSave"
+        val dir = File(path)
+
+        if(dir.exists().not()) {
+            dir.mkdirs() // 폴더 없을경우 폴더 생성
+        }
+
+        try {
+            val fileItem = File("$dir/$fileName")
+            fileItem.createNewFile()
+            //0KB 파일 생성.
+
+            val fos = FileOutputStream(fileItem) // 파일 아웃풋 스트림
+
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+            //파일 아웃풋 스트림 객체를 통해서 Bitmap 압축.
+
+            fos.close() // 파일 아웃풋 스트림 객체 close
+
+            sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(fileItem)))
+            // 브로드캐스트 수신자에게 파일 미디어 스캔 액션 요청. 그리고 데이터로 추가된 파일에 Uri를 넘겨준다.
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
